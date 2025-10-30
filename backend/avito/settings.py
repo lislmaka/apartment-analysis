87e38a997b14de6eda09ba95269e225df59fc47a
Andrey <lislmaka@>
2025-10-30 21:28:22 +0300

    Настройка соединения между nuxtjs и django

--- backend/avito/settings.py
+++ backend/avito/settings.py
@@ -138,3 +138,10 @@ STATIC_ROOT = BASE_DIR / "static/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:1337", "http://127.0.0.1:1337"]
+
+
+# CORS_ORIGIN_WHITELIST = [
+#     'service.frontend',
+# ]
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ORIGIN_ALLOW_ALL = True
--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+const { data } = await useFetch('http://service.backend:8000/api/')
+// const { data } = await useFetch('https://fakeapi.extendsclass.com/books')
+
+</script>
+
 <template>
-  <div>
-    Hello and welcome
-  </div>
+  <p>Page visits: {{ data }}</p>
 </template>
