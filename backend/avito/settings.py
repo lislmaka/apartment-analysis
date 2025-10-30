c03e1c0663d46e53eac0352983358d3182c504fa
Andrey <lislmaka@>
2025-10-30 15:41:41 +0300

    изменил пить к файлу базы sqlite3

--- backend/avito/settings.py
+++ backend/avito/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'avito.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db/my_database.db',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
